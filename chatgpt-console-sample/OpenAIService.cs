685847f717de276b628ff67cf83c63cc1b4f87e2
Juan Pablo Carrasco <juan.carrasco@hp.com>
2023-02-18 09:52:17 -0500

    adding simple sample project that consumes ChatGPT davinci API - most code was generated by ChatGPT

---
+++ chatgpt-console-sample/OpenAIService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Authentication;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+public class OpenAIService
+{
+    private readonly string _apiKey;
+    private readonly HttpClient _httpClient;
+
+    public OpenAIService(string apiKey)
+    {
+        var _handler = new HttpClientHandler();
+        _handler.SslProtocols = SslProtocols.Tls12 | SslProtocols.Tls11 | SslProtocols.Tls;
+        _apiKey = apiKey;
+        _httpClient = new HttpClient(_handler);
+        _httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", _apiKey);
+    }
+
+    public async Task<string> GetCompletionAsync(string prompt)
+    {
+        var requestBody = new
+        {
+            prompt = prompt,
+            temperature = 0.5,
+            max_tokens = 50,
+            top_p = 1,
+            frequency_penalty = 0,
+            presence_penalty = 0
+        };
+        
+        _httpClient.BaseAddress = new Uri("https://api.openai.com/v1/engines/davinci-codex/completions");
+        var response = await _httpClient.PostAsJsonAsync(_httpClient.BaseAddress,requestBody);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+
+        var jsonResponse = JsonDocument.Parse(responseContent);
+
+        return jsonResponse.RootElement.GetProperty("choices")[0].GetProperty("text").GetString();
+    }
+}
---
+++ chatgpt-console-sample/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Ask me a question or type 'exit' to quit:");
+
+// Initialize an HTTP client to make requests to the OpenAI API
+var httpClient = new HttpClient();
+
+while (true)
+{
+    Console.Write("> ");
+    var input = Console.ReadLine();
+
+    if (input.ToLower() == "exit")
+    {
+        break;
+    }
+
+    var openAiService = new OpenAIService("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");
+
+    var completion = await openAiService.GetCompletionAsync(input);
+
+    Console.WriteLine(completion);
+}
