fecacfca7b06bbf330ac8b1b39352c485b7738ab
Juan Pablo Carrasco <juacasco@gmail.com>
2023-07-02 22:08:35 -0500

    Updates to latest based on docs
    Updates to fix reported errors 404 --- old one may have been removed

--- chatgpt-console-sample/Program.cs
+++ chatgpt-console-sample/Program.cs
@@ -1,22 +1,36 @@
 ﻿
-Console.WriteLine("Ask me a question or type 'exit' to quit:");
+// Create an instance of the configuration object
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 
-// Initialize an HTTP client to make requests to the OpenAI API
-var httpClient = new HttpClient();
+var config = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).Build();
 
+// Replace with your desired model ID
+var modelId = "gpt-3.5-turbo";
+
+// Create an instance of the OpenAIService class using dependency injection
+var service = new OpenAIService(config, modelId);
+
+// Start a while loop that continues until the user enters "exit"
 while (true)
 {
     Console.Write("> ");
     var input = Console.ReadLine();
 
-    if (input.ToLower() == "exit")
-    {
+    // If the user enters "exit", break out of the loop
+    if (input == "exit")
         break;
-    }
 
-    var openAiService = new OpenAIService("<TokenHere>");
+    // Otherwise, create a list of messages comprising the conversation so far
+    var messages = new[]
+    {
+        new { role = "system", content = "You are a helpful assistant." },
+        new { role = "user", content = input }
+    };
 
-    var completion = await openAiService.GetCompletionAsync(input);
+    // Call the GetCompletionAsync method to get a chat completion
+    var response = await service.GetCompletionAsync(JsonSerializer.Serialize(messages));
 
-    Console.WriteLine(completion);
+    // Print the response content to the console
+    Console.WriteLine(response);
 }
